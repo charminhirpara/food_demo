dd8aa419b3b7012cf0606942483d154c8a7df196
Nikhil <nikhilboghara1339@gmail.com>
2023-09-11 17:46:09 +0530

    1

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -3,8 +3,7 @@
 class Checkout < ApplicationRecord
   belongs_to :cart
   belongs_to :product
-
-  def subtotal
+  
     product.price * quantity
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,8 @@
 
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_foods
+  has_many :order_foods, :dependent => :delete_all
+  validates :sub_total, :total, numericality: { greater_than_or_equal_to: 1 }
   def self.ransackable_attributes(_auth_object = nil)
     %w[address city created_at discounnt email id phone_number sub_total total updated_at
        user_id]
